d0048c8ce76cf3dfc1a555c17ffa992a76e6f2ca
HAROONABUTABAR <haroonabutabar9966@gmail.com>
2022-04-26 22:58:39 +0000

    training

---
+++ NumericDataTypes.py
@@ -0,0 +1,21 @@
+print("Python has three numeric types: int, float, and complex")
+myValueOne=1
+print(myValueOne)
+print(type(myValueOne))
+print(str(myValueOne) + " is of the data type " + str(type(myValueOne)))
+myValueTwo=3.14
+print(myValueTwo)
+print(type(myValueTwo))
+print(str(myValueTwo) + " is of the data type " + str(type(myValueTwo)))
+myValueThree=5j
+print(myValueThree)
+print(type(myValueThree))
+print(str(myValueThree) + " is of the data type " + str(type(myValueThree)))
+myValueFuor=True
+print(myValueFuor)
+print(type(myValueFuor))
+print(str(myValueFuor) + " is of the data type " + str(type(myValueFuor)))
+myValueFive=True
+print(myValueFive)
+print(type(myValueFive))
+print(str(myValueFive) + " is of the data type " + str(type(myValueFive)))
